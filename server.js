a3332fafceb02cc992a54015e43e104c8903948b
Michael O'Connor <oconnor97@aol.com>
2021-04-03 13:30:46 -0400

    changing public

--- server.js
+++ server.js
@@ -14,23 +14,23 @@ const reservations = []
 
 
 // Basic route that sends the user first to the AJAX Page
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/view.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'view.html')));
 app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
 app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
 
-app.get('/api/tables', (req, res) => res.json(reservations.slice(0,5)));
+app.get('/api/tables', (req, res) => res.json(reservations.slice(0, 5)));
 app.get('/api/waitlist', (req, res) => res.json(reservations.slice(5)))
 
 app.post('/api/tables', (req, res) => {
     const newReservation = req.body;
     console.log(newReservation)
 
-    reservations.push(newReservation);    
-    res.json(reservations.length < 5); 
+    reservations.push(newReservation);
+    res.json(reservations.length < 5);
 });
 
 
-app.post('/api/clear', (req,res) =>{
+app.post('/api/clear', (req, res) => {
     const newReservation = req.body;
 
     res.json(reservations.empty());
