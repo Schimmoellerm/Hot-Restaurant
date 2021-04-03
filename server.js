225ea8c855619bfa496f7109d6c53382a2be6830
Morgan Schall <morgan.allison.schall@gmail.com>
2021-04-03 13:11:30 -0400

    it works

--- server.js
+++ server.js
@@ -10,15 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const reservations = [
-    {
-        routeName: '',
-        name: '',
-        phoneNum: '',
-        email: '',
-        uniqueID: '',
-      },
-]
+const reservations = []
 
 
 // Basic route that sends the user first to the AJAX Page
@@ -26,9 +18,23 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'view.html')));
 app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
 app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
 
-app.post('/api/characters', (req, res) => {
+app.get('/api/tables', (req, res) => res.json(reservations.slice(0,5)));
+app.get('/api/waitlist', (req, res) => res.json(reservations.slice(5)))
 
-  });
+app.post('/api/tables', (req, res) => {
+    const newReservation = req.body;
+    console.log(newReservation)
+
+    reservations.push(newReservation);    
+    res.json(reservations.length < 5); 
+});
+
+
+app.post('/api/clear', (req,res) =>{
+    const newReservation = req.body;
+
+    res.json(reservations.empty());
+})
 
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
